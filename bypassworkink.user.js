8f33fe8a54a57f29deb18a7f563825b0bb0190bf
nhantran11325-netizen <nhantran11325@gmail.com>
2025-09-28 07:12:31 +0700

    Add files via upload

---
+++ bypassworkink.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         work.ink bypass vip
+// @namespace    http://tampermonkey.net/
+// @version      2025-09-11
+// @description  bypasses work.ink shortened links
+// @author       fqzzdx
+// @match        https://work.ink/*
+// @run-at       document-start
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=work.ink
+// @downloadURL  https://raw.githubusercontent.com/nhantran11325-netizen/bypass-work-ink/refs/heads/main/bypass.js
+// @updateURL    https://raw.githubusercontent.com/nhantran11325-netizen/bypass-work-ink/refs/heads/main/bypass.js
+// ==/UserScript==
